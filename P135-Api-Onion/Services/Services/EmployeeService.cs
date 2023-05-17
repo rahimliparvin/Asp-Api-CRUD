32d90965d4c89acca7fc19b1912777e97d62c04d
rahimliparvin <parvinvr@code.edu.az>
2023-05-17 13:39:11 -0700

    add homework

---
+++ P135-Api-Onion/Services/Services/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Repository.Repositories.Interfaces;
+using Services.DTOs.Employee;
+using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepo;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepo, IMapper mapper)
+        {
+            _employeeRepo = employeeRepo;
+            _mapper = mapper;
+        }
+
+        public async Task CreateAsync(EmployeeCreateDto employee) => await _employeeRepo.CreateAsync(_mapper.Map<Employee>(employee));
+
+        public async Task<IEnumerable<EmployeeDto>> GetAllAsync() => _mapper.Map<IEnumerable<EmployeeDto>>(await _employeeRepo.GetAllAsync());
+
+        public async Task<EmployeeDto> GetByIdAsync(int? id) => _mapper.Map<EmployeeDto>(await _employeeRepo.GetByIdAsync(id));
+
+        public async Task DeleteAsync(int? id) => await _employeeRepo.DeleteAsync(await _employeeRepo.GetByIdAsync(id));
+
+        public async Task UpdateAsync(int id, EmployeeUpdateDto employee)
+        {
+
+            var dbEmployee = await _employeeRepo.GetByIdAsync(id);
+
+            _mapper.Map(employee, dbEmployee);
+
+            await _employeeRepo.UpdateAsync(dbEmployee);
+
+        }
+
+        public Task Search(string name, EmployeeSearchDto employee)
+        {
+            throw new NotImplementedException();
+        }
+
+   
+    }
+}
